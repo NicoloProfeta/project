2f36de72afd36086014c1f579821beec4ba22028
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-03-27 16:52:16 +0200

    Update task4datamuse.py

--- task4datamuse.py
+++ task4datamuse.py
@@ -17,21 +17,21 @@ import streamlit as st
 # make the above URL more generic, so that it is easy to replace the keyword
 keyword=st.text_input('Please, insert a keyword ')
 option = st.selectbox('select a function', ('synonyms', 'antonyms', 'sounds like', 'means like'))
-
-url= 'https://api.datamuse.com/words?rel_syn=' + keyword
+if keyword and option:
+ url= 'https://api.datamuse.com/words?rel_syn=' + keyword
 
 
 #Step3: Download the JSON data from the API.
-response = requests.get(url)   
+ response = requests.get(url)   
 #Uncomment to see the raw JSON text:
 #print(response.text)  
 
 
 #Step4: Load JSON data into a Python variable and use it in your program.
-dataFromDatamuse = json.loads(response.text) 
+ dataFromDatamuse = json.loads(response.text) 
 #Uncomment to see the raw JSON text loaded in a Python Variable:
 #print(dataFromDatamuse) 
 #Uncomment to see a better readable version:
-st.write(dataFromDatamuse)
+ st.write(dataFromDatamuse)
 
-st.write('You selected:', option)
+ st.write('You selected:', option)
