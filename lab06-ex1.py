e2ebac3fbabbb4403fb28f114e99edad4b050d1e
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-04-11 16:41:42 +0200

    Update lab06-ex1.py

--- lab06-ex1.py
+++ lab06-ex1.py
@@ -6,9 +6,10 @@ import streamlit as st
 translator = Translator()
 word = st.text_input('Type some text:')
 language=st.text_input('Type in a language')
+translation=translator.translate(word,dest=language)
 
 
-tts1=gTTS(text=languageis.text, lang=language) 
+tts1=gTTS(text=translation.text, lang=language) 
 tts1.save('user.mp3')
 
 print('auf Ru:')
