6e20acc28e5217a9c66be98abd6d9e6dce1c0b33
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-04-11 16:56:44 +0200

    Create lab06-ex2.py

---
+++ lab06-ex2.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+
+from googletrans import Translator
+import streamlit as st
+
+translator = Translator()
+word = st.text_input('Type some text:')
+language=st.text_input('Type in a language')
+translation=translator.translate(word,dest=language)
+
+
+tts1=gTTS(text=translation.text, lang=language) 
+tts1.save('user.mp3')
+
+print('auf Ru:')
+st.audio('user.mp3')
+
+st.download_button(label="Download audio file",
+data=audio_file, file_name='yourfile.mp3',mime='audio/mp3')
