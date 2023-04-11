3b102c56c4b8abdfc61130a4aacaf89dbd3df25f
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-04-11 16:18:44 +0200

    Update and rename task3-11.04.py to lab06-ex1.py

--- lab06-ex1.py
+++ lab06-ex1.py
@@ -5,9 +5,9 @@ from googletrans import Translator
 translator = Translator()
 word = st.texct_input('Type some text:')
 
-languageis = translator.translate(word, dest='ru') #---> lingua che viene tradotta da un'altra lingua
+languageis = translator.translate(word, dest='ru') 
 
-tts1=gTTS(text=languageis.text, lang='ru') #--> lingua pronunciata
+tts1=gTTS(text=languageis.text, lang='ru') 
 tts1.save('user.mp3')
 
 print('auf Ru:')
