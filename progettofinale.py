981471faf0d1163ff26dea1644ebb20205fe896d
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-08 16:54:08 +0200

    Update progettofinale.py

--- progettofinale.py
+++ progettofinale.py
@@ -6,12 +6,12 @@ import streamlit as st
 
 client = GrammarBotClient()
 
-client = GrammarBotClient(api_key= '09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331')
-#APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
+client = GrammarBotClient(api_key='APIkey')
+APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
 Text = st.text_input("Schreib deinen Text")
 url = "https://grammarbot-neural.p.rapidapi.com/v1/check" + Text + '&appid=' + APIkey
 #print(url)
 
-res = client.check(text)
+res = client.check(Text)
 
 
