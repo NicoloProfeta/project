be969d600684603a2698b6756b763661c130f1a4
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-08 16:17:14 +0200

    Update progettofinale.py

--- progettofinale.py
+++ progettofinale.py
@@ -6,6 +6,6 @@ import streamlit as st
 APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
 Text = st.text_input("Schreib deinen Text")
 url = "https://grammarbot-neural.p.rapidapi.com/v1/check" + Text + '&appid=' + APIkey
-print(url)
+#print(url)
 
 res = Text.check(text)
