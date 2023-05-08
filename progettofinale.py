de0b0be3864fc30d52cbc72fcd25d38017b83e28
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-08 15:53:24 +0200

    Create progettofinale.py

---
+++ progettofinale.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+import requests
+
+APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
+Text = st.text_input("Schreib deinen Text")
+url = "https://grammarbot-neural.p.rapidapi.com/v1/check" + Text + '&appid=' + APIkey
+print(url)
+
+response = requests.get(url)
+grammarbot = json.loads(response.text)
