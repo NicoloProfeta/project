56258e9c4b1e6eb3c6686c7777c6d01e89ad9c61
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-08 16:58:16 +0200

    Update progettofinale.py

--- progettofinale.py
+++ progettofinale.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 client = GrammarBotClient()
 
-client = GrammarBotClient(api_key='APIkey')
 APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
+client = GrammarBotClient(api_key='APIkey')
 Text = st.text_input("Schreib deinen Text")
 url = "https://grammarbot-neural.p.rapidapi.com/v1/check" + Text + '&appid=' + APIkey
 #print(url)
