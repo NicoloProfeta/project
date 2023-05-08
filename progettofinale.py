05828ffedd2adff20a64b61ac9594424f8ed36a7
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-08 16:28:56 +0200

    Update progettofinale.py

--- progettofinale.py
+++ progettofinale.py
@@ -3,6 +3,8 @@ import json
 
 import streamlit as st
 
+client = GrammarBotClient()
+
 client = GrammarBotClient(api_key='APIkey')
 APIkey = "09972ee78cmsh877ccbbcbd94f02p13fc75jsn53d32a153331"
 Text = st.text_input("Schreib deinen Text")
