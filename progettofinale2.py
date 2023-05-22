0681a667b50635337f158e0469fe6dbdbe54e1f9
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-22 23:05:58 +0200

    Update progettofinale2.py

--- progettofinale2.py
+++ progettofinale2.py
@@ -1,28 +1,43 @@
-import requests
-import json
-
 import streamlit as st
-
+import grammar_check
+from gtts import gTTS
+from io import BytesIO
+from IPython.display import Audio
 
 def revise_homework(text):
-    # Implement your revision logic here
-    # This is just a placeholder implementation that adds " [Revised]" to the end of the text
-    revised_text = text.strip() + " [Korrigiert]"
+    # Use GrammarBot to correct orthography
+    tool = grammar_check.LanguageTool('en-US')
+    matches = tool.check(text)
+    revised_text = grammar_check.correct(text, matches)
     return revised_text
 
+def generate_pronunciation(text):
+    # Generate pronunciation audio using gTTS
+    tts = gTTS(text, lang='de')
+    audio = BytesIO()
+    tts.save(audio)
+    return audio
+
 def main():
-    print("Willkommen zum Peer Review APP!")
-    print("Schreib bitte deinen Text (500 Wörter oder weniger):")
-    text = input()
+    st.title("Homework Revision Program")
+    st.write("Please enter your text (500 characters or less):")
+
+    # Create a text input box for the user to enter the text
+    text = st.text_area("Enter your text", height=200)
 
     # Check if text exceeds the length limit
     if len(text) > 500:
-        print("Dein Text enthält mehr als 500 Wörter! Versuch es noch einmal!")
+        st.error("Text exceeds the length limit of 500 characters. Please try again.")
         return
 
-    revised_text = revise_homework(text)
-    print("\nRevised Homework:")
-    print(revised_text)
+    if st.button("Revise Homework"):
+        revised_text = revise_homework(text)
+        st.write("\nRevised Homework:")
+        st.write(revised_text)
+
+        if st.button("Hear Pronunciation"):
+            audio = generate_pronunciation(revised_text)
+            st.audio(audio)
 
 if __name__ == "__main__":
     main()
