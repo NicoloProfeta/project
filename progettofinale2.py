7bf4b81dae2c253da9e0530c3d6c847e4752ee93
NicoloProfeta <127765642+NicoloProfeta@users.noreply.github.com>
2023-05-31 12:00:46 +0200

    Update progettofinale2.py

--- progettofinale2.py
+++ progettofinale2.py
@@ -7,7 +7,7 @@ def correct_german_text(text):
     #parser = GingerIt()
     #corrected = parser.parse(text)['result']
     #return corrected
-    tool = language_tool_python.LanguageToolPublicAPI('it')
+    tool = language_tool_python.LanguageToolPublicAPI('de-DE')
     return tool.correct(text)
     
 
